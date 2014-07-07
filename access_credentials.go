d709980bbc5fcca18cfeda1feb42707f0825cdbe
PGCT1 <t1gemini@yandex.ru>
2014-07-08 03:18:03 +0400

    test

--- access_credentials.go
+++ access_credentials.go
@@ -3,4 +3,4 @@ package main
 const TwitterApiKey = "YOUR_API_KEY"
 const TwitterApiSecret = "YOUR_API_SECRET"
 const TwitterAccessToken = "YOUR_ACCESS_TOKEN"
-const TwitterAccessTokenSecret = "YOUR_ACCESS_SECRaET"
+const TwitterAccessTokenSecret = "YOUR_ACCESS_SECRET"
